8eea9603547f606b0dcabeb6168921220b28eb49
Eben Gunadi <egunadi@usc.edu>
2023-10-25 21:29:31 -0700

    Have js/proportional-symbol-map.js pull data dynamically

--- js/proportional-symbol-map.js
+++ js/proportional-symbol-map.js
@@ -26,47 +26,49 @@ function createProportionalSymbolMap() {
             .style("fill", "rgba(0, 0, 0, 0.1)"); // Adjust the opacity as needed
     });
 
-    // Add bubbles to the map
-    var bubbleData = [{"Year":2020,"Country":"Japan","Population":125244.761,"radius":35.3899365639,"lat":36.2048,"lon":138.2529},{"Year":2020,"Country":"Korea","Population":51844.69,"radius":22.7694290662,"lat":35.9078,"lon":127.7669},{"Year":2020,"Country":"Indonesia","Population":271857.97,"radius":52.140000959,"lat":-0.7893,"lon":113.9213},{"Year":2020,"Country":"China","Population":1424929.781,"radius":119.3704226766,"lat":35.8617,"lon":104.1954},{"Year":2020,"Country":"Singapore","Population":5909.869,"radius":7.6875672355,"lat":1.3521,"lon":103.8198}];
+    // Load data from JSON file
+    d3.json("data/population_2020_circles.json").then(function(bubbleData) {
+        const countries = Array.from(new Set(bubbleData.map(d => d.Country))); // Get unique countries
 
-    const countries = Array.from(new Set(bubbleData.map(d => d.Country))); // Get unique countries
+        // Define a color scale
+        var colorScale = d3.scaleOrdinal()
+            .domain(countries)
+            .range(d3.schemeTableau10);
 
-    // Define a color scale
-    var colorScale = d3.scaleOrdinal()
-        .domain(countries)
-        .range(d3.schemeTableau10);
+        var bubbleGroup = svgMap.append("g");
 
-    var bubbleGroup = svgMap.append("g");
+        bubbleGroup.selectAll("circle")
+            .data(bubbleData.sort((a, b) => (["Korea", "Singapore"].includes(a.Country)) ? 1 : -1)) // Sort data so Korea and Singapore are processed last
+            .enter()
+            .append("circle")
+            .attr("cx", function (d) { return projection([d.lon, d.lat])[0]; })
+            .attr("cy", function (d) { return projection([d.lon, d.lat])[1]; })
+            .attr("r", function (d) { return d.radius; })
+            .attr("fill", function (d) { return colorScale(d.Country); })
+            .attr("stroke", "#fff")
+            .attr("stroke-width", 2);
 
-    bubbleGroup.selectAll("circle")
-        .data(bubbleData.sort((a, b) => (["Korea", "Singapore"].includes(a.Country)) ? 1 : -1)) // Sort data so Korea and Singapore are processed last
-        .enter()
-        .append("circle")
-        .attr("cx", function (d) { return projection([d.lon, d.lat])[0]; })
-        .attr("cy", function (d) { return projection([d.lon, d.lat])[1]; })
-        .attr("r", function (d) { return d.radius; })
-        .attr("fill", function (d) { return colorScale(d.Country); })
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 2);
+        // Create a legend for the population scale
+        var legend = svgMap.append("g")
+            .attr("class", "legend")
+            .attr("transform", "translate(20, 20)"); // Adjust the position of the legend
 
-    // Create a legend for the population scale
-    var legend = svgMap.append("g")
-        .attr("class", "legend")
-        .attr("transform", "translate(20, 20)"); // Adjust the position of the legend
+        var legendScale = d3.scaleSqrt()
+            .domain([0, d3.max(bubbleData, function(d) { return d.Population; })])
+            .range([0, 200]); // Increase the range of the legend size to space out the scale numbers vertically
 
-    var legendScale = d3.scaleSqrt()
-        .domain([0, d3.max(bubbleData, function(d) { return d.Population; })])
-        .range([0, 200]); // Increase the range of the legend size to space out the scale numbers vertically
+        var legendAxis = d3.axisRight(legendScale)
+            .ticks(5)
+            .tickFormat(d3.format(".2s")); // Format tick values as human-readable numbers
 
-    var legendAxis = d3.axisRight(legendScale)
-        .ticks(5)
-        .tickFormat(d3.format(".2s")); // Format tick values as human-readable numbers
-
-    legend.append("g")
-        .attr("class", "legendSize")
-        .attr("transform", "translate(20, 0)")
-        .call(legendAxis)
-        .selectAll("text")
-        .style("font-size", "12px"); // Adjust font size of the tick labels
-
+        legend.append("g")
+            .attr("class", "legendSize")
+            .attr("transform", "translate(20, 0)")
+            .call(legendAxis)
+            .selectAll("text")
+            .style("font-size", "12px"); // Adjust font size of the tick labels
+    }).catch(function(error) {
+        // Handle errors if the JSON file fails to load
+        console.error("Error loading the data: " + error);
+    });
 }
