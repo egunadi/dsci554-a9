a7ea8db749f5e0c1c1fe7c4c8faf674bc525cf48
Eben Gunadi <egunadi@usc.edu>
2023-10-26 13:27:28 -0700

    Clean up files

--- js/choropleth-map.js
+++ js/choropleth-map.js
@@ -8,54 +8,74 @@ function createChoroplethMap() {
         .attr("width", widthMap)
         .attr("height", heightMap);
 
-    // Define a projection centered on Asia with a decreased scale for more zoom out
+    // Map and projection
+    var path = d3.geoPath();
     var projection = d3.geoMercator()
-        .scale(300) // Decreased scale for greater zoom out
+        .scale(280) // Decreased scale for greater zoom out
         .translate([widthMap / 2, heightMap / 2])
         .center([110, 25]); // Adjusted center coordinates
 
-    var path = d3.geoPath().projection(projection);
+    // Data and color scale
+    var data = d3.map();
+    var colorScale = d3.scaleThreshold()
+      .domain([100000, 30000000, 500000000])
+      .range(d3.schemeBlues[3]);
 
-    // Load your data for the choropleth map
-    var data = [
-        {"Country": "Japan", "Population": 125244.761},
-        {"Country": "Korea", "Population": 51844.69},
-        {"Country": "Indonesia", "Population": 271857.97},
-        {"Country": "China", "Population": 1424929.781},
-        {"Country": "Singapore", "Population": 5909.869}
-    ];
+    // Load external data and boot
+    d3.queue()
+      .defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
+      .defer(d3.csv, "data/world_population.csv", function(d) { data.set(d.code, +d.pop); 
+          // console.log(d.code, +d.pop); // Add this line to debug the data loading
+      })
+      .await(ready);
 
-    // Define a color scale based on your data values
-    var colorScale = d3.scaleSequential(d3.interpolateBlues) // You can choose a different color scale
-        .domain([0, d3.max(data, function(d) { return d.Population; })]);
+    function ready(error, topo) {
 
-    var countryMapping = {
-        "JPN": "Japan",
-        "KOR": "Korea", // Corrected the name from "South Korea" to match GeoJSON data
-        "IDN": "Indonesia",
-        "CHN": "China",
-        "SGP": "Singapore"
-    };
+      // Draw the map
+      svgMap.append("g")
+        .selectAll("path")
+        .data(topo.features)
+        .enter()
+        .append("path")
+          // draw each country
+          .attr("d", d3.geoPath()
+            .projection(projection)
+          )
+          // set the color of each country
+          .attr("fill", function (d) {
+            d.total = data.get(d.id) || 0;
+            return colorScale(d.total);
+          });
 
-    // Load your data for the choropleth map
-    d3.json("https://raw.githubusercontent.com/d3/d3.github.com/master/world-110m.v1.json").then(function (world) {
-        // Log country names from the GeoJSON data
-        console.log("GeoJSON Countries:", topojson.feature(world, world.objects.countries).features.map(d => d.properties.name));
-        // Draw the world map with choropleth coloring
-        svgMap.selectAll("path")
-            .data(topojson.feature(world, world.objects.countries).features)
-            .enter().append("path")
-            .attr("d", path)
-            .attr("class", "country")
-            .style("fill", function(d) {
-                // Lookup the data value for the current country
-                var countryData = data.find(function(country) {
-                    return countryMapping[d.id] === country.Country; // Standardize the name for comparison
-                });
-                // Use the color scale to determine the fill color
-                return countryData ? colorScale(countryData.Population) : "lightgray"; // Default color for missing data
-            });
-    }).catch(function(error) {
-        console.error("Error loading world data:", error); // Log any errors during world data loading
-    });
+      // Create a legend
+      var legendColors = colorScale.range();
+      var legendWidth = 50;
+      var legendHeight = 20;
+
+      // Define legend labels
+      var legendLabels = ['< 100k', '30M', '> 500M'];
+
+      var legend = svgMap.append("g")
+          .attr("class", "legend")
+          .attr("transform", "translate(5,360)");
+
+      legend.selectAll("rect")
+          .data(legendColors)
+          .enter().append("rect")
+          .attr("x", function(d, i) { return i * legendWidth; })
+          .attr("y", 0)
+          .attr("width", legendWidth)
+          .attr("height", legendHeight)
+          .style("fill", function(d) { return d; });
+
+      // Adjust the x-coordinate calculation for text
+      legend.selectAll("text")
+          .data(legendLabels)
+          .enter().append("text")
+          .attr("x", function(d, i) { return i * legendWidth + legendWidth / 2; }) // Center the text within each rectangle
+          .attr("y", legendHeight + 15)
+          .style("text-anchor", "middle")
+          .text(function(d) { return d; });
+
+    }
 }
