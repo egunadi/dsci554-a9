56b850037741a9772433d302305b7c719caab224
Eben Gunadi <egunadi@usc.edu>
2023-10-27 20:54:19 -0700

    Add dot-map.html

--- js/dot-map.js
+++ js/dot-map.js
@@ -1,8 +1,8 @@
-function createProportionalSymbolMap() {
-    var widthMap = document.getElementById("proportional-symbol-map").offsetWidth;
+function createDotMap() {
+    var widthMap = document.getElementById("dot-map").offsetWidth;
     var heightMap = 400;
 
-    var svgMap = d3.select("#proportional-symbol-map")
+    var svgMap = d3.select("#dot-map")
         .html("") // Clear the existing map
         .append("svg")
         .attr("width", widthMap)
@@ -26,47 +26,28 @@ function createProportionalSymbolMap() {
             .style("fill", "rgba(0, 0, 0, 0.1)"); // Adjust the opacity as needed
     });
 
+    const countries = ['Japan', 'Korea', 'Indonesia', 'China', 'Singapore'];
+
+    // Define a color scale
+    var colorScale = d3.scaleOrdinal()
+        .domain(countries)
+        .range(d3.schemeTableau10);
+
+    var dotGroup = svgMap.append("g");
+
     // Load data from JSON file
-    d3.json("data/population_2020_circles.json").then(function(bubbleData) {
-        const countries = Array.from(new Set(bubbleData.map(d => d.Country))); // Get unique countries
-
-        // Define a color scale
-        var colorScale = d3.scaleOrdinal()
-            .domain(countries)
-            .range(d3.schemeTableau10);
-
-        var bubbleGroup = svgMap.append("g");
-
-        bubbleGroup.selectAll("circle")
-            .data(bubbleData.sort((a, b) => (["Korea", "Singapore"].includes(a.Country)) ? 1 : -1)) // Sort data so Korea and Singapore are processed last
+    d3.json("data/cities.json").then(function(cities) {
+        // Render dots for cities
+        dotGroup.selectAll("circle")
+            .data(cities.sort((a, b) => a.country === "Singapore" ? 1 : -1)) // Sort data so Singapore is processed last
             .enter()
             .append("circle")
-            .attr("cx", function (d) { return projection([d.lon, d.lat])[0]; })
-            .attr("cy", function (d) { return projection([d.lon, d.lat])[1]; })
-            .attr("r", function (d) { return d.radius; })
-            .attr("fill", function (d) { return colorScale(d.Country); })
+            .attr("cx", function (d) { return projection([d.lng, d.lat])[0]; })
+            .attr("cy", function (d) { return projection([d.lng, d.lat])[1]; })
+            .attr("r", 5) // Adjust the radius of the dots as needed
+            .attr("fill", function (d) { return colorScale(d.country); })
             .attr("stroke", "#fff")
-            .attr("stroke-width", 2);
-
-        // Create a legend for the population scale
-        var legend = svgMap.append("g")
-            .attr("class", "legend")
-            .attr("transform", "translate(20, 20)"); // Adjust the position of the legend
-
-        var legendScale = d3.scaleSqrt()
-            .domain([0, d3.max(bubbleData, function(d) { return d.Population; })])
-            .range([0, 200]); // Increase the range of the legend size to space out the scale numbers vertically
-
-        var legendAxis = d3.axisRight(legendScale)
-            .ticks(5)
-            .tickFormat(d3.format(".2s")); // Format tick values as human-readable numbers
-
-        legend.append("g")
-            .attr("class", "legendSize")
-            .attr("transform", "translate(20, 0)")
-            .call(legendAxis)
-            .selectAll("text")
-            .style("font-size", "12px"); // Adjust font size of the tick labels
+            .attr("stroke-width", 1);
     }).catch(function(error) {
         // Handle errors if the JSON file fails to load
         console.error("Error loading the data: " + error);
